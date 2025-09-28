42f376cfb54f6b5c98f62d0506bf3717eb596b3b
turgt <turgutsimarmaz@gmail.com>
2025-09-28 04:01:34 +0300

    update

--- src/db/seeders/20250928000748_admin_user.cjs
+++ src/db/seeders/20250928000748_admin_user.cjs
@@ -3,14 +3,22 @@ const bcrypt = require('bcrypt');
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    const hash = await bcrypt.hash('admin123', 10);
-    await queryInterface.bulkInsert('Users', [{
-      username: 'admin',
-      passwordHash: hash,
-      isAdmin: true,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }]);
+    const [results] = await queryInterface.sequelize.query(
+      "SELECT id FROM \"Users\" WHERE username = 'admin' LIMIT 1;"
+    );
+
+    if (results.length === 0) {
+      const hash = await bcrypt.hash('admin123', 10);
+      await queryInterface.bulkInsert('Users', [{
+        username: 'admin',
+        passwordHash: hash,
+        isAdmin: true,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }]);
+    } else {
+      console.log("⚠️ Admin user already exists, skipping seeder.");
+    }
   },
 
   async down (queryInterface, Sequelize) {
